acc1c409bee3893bbd90458603f685376331f7c0
rachelbt <rachelbt@users.noreply.github.com>
2017-01-23 13:53:42 +0200

    first upload

---
+++ lotto-script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by RachelB on 22/01/2017.
+ */
+function generatNumber() {
+    document.myform.one.value = Math.floor(Math.random()*11);
+    document.myform.two.value = Math.floor(Math.random()*11);
+    document.myform.three.value = Math.floor(Math.random()*11);
+    document.myform.four.value = Math.floor(Math.random()*11);
+    document.myform.five.value = Math.floor(Math.random()*11);
+    document.myform.six.value = Math.floor(Math.random()*11);
+}
+
